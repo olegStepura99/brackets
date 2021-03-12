3d476e9f251695c707495d3e89a5cacebf6efaef
olegStepura99 <79783293+olegStepura99@users.noreply.github.com>
2021-03-12 12:04:24 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   let stack = [];
+  let keys = [];
+  let value = [];
+  let obj = [];
+
+
+  if (str.length % 2 ===1){
+    return false 
+  }
+
+  for(let i = 0; i <bracketsConfig.length; i++){
+    keys.push(bracketsConfig[i][0])
+    value.push(bracketsConfig[i][i]);
+    obj[bracketsConfig[i][0]] = bracketsConfig[i][i];
+  }
+
+  for(let i = o; i < str.length; i++){
+    if(keys.includes(str[i]) && value.includes(str[i])){
+      if(stack[stack.length -1 ] === str[i]){
+        stack.pop();
+      }else{
+        stack.push(str[i]);
+      }
+    }else if(keys.includes(str[i])){
+      stack.push(str[i]);
+    }else if(value.includes(str[i])){
+      if(obj[stack[stack.length - 1]] === str[i]){
+        stack.pop();
+      }else{
+        stack.push('1');
+      }
+    }
+  }
+
+  if(stack.length===0){
+    return true;
+  }else{
+    return false;
+  }
+
 }
